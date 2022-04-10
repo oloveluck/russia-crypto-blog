d9e626d0e4d99bba4189889272333c582915a0aa
evanritzcovan <evanritzcovan@gmail.com>
2022-04-10 19:32:30 -0400

    editing home page button text

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            What we're all about
           </Link>
         </div>
       </div>
